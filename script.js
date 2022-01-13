5e5766cda061911e12c1acd2201ae7170cfbd557
Evellyn Laurenti <95755701+elaurenti@users.noreply.github.com>
2022-01-13 09:50:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,16 +10,25 @@ let userPalpite = document.querySelector('.userPalpite');
 let mensagemFimDeJogo = document.querySelector('.mensagemFimDeJogo');
 let mensagemPalpites = document.querySelector('.mensagemPalpites');
 let botaoReinicio = document.querySelector('.reiniciarGame');
+let mensagemStatus =  document.querySelector('.mensagemStatus');
 
 let maximoTentativas = 10;
 let tentativaAtual = 1;
 
-
 function checarPalpite() {
     let valorCampo = Number(userPalpite.value);
-    
-        mensagemPalpites.textContent = 'Tentativas: '+ tentativaAtual;
-    
+    mensagemPalpites.textContent = 'Tentativas: ' + tentativaAtual;
+
+    if (userPalpite.value == false) {
+        mensagemStatus.textContent =  'Digite algum valor';
+        mensagemStatus.style.backgroundColor = 'red';
+        return;
+      } 
+
+    else {
+        mensagemStatus.textContent = '';
+    }
+
     if (valorCampo === numeroAleatorio) {
         resultadoFinal.textContent = 'Parabéns! Você acertou!';
         resultadoFinal.style.backgroundColor = 'green';
@@ -40,15 +49,17 @@ function checarPalpite() {
     } else {
         resultadoFinal.textContent = 'Errado!';
         resultadoFinal.style.backgroundColor = 'red';
+
         if (valorCampo < numeroAleatorio) {
-            altoOuBaixo.textContent = "Seu palpite foi muito baixo!"
+            altoOuBaixo.textContent = "Seu palpite foi muito baixo!";
 
         } else {
-            altoOuBaixo.textContent = "Seu palpite foi muito alto!"
+            altoOuBaixo.textContent = "Seu palpite foi muito alto!";
 
-        }
+        } 
+        
+    } 
 
-    }
     tentativaAtual++;
 }
 envioPalpite.addEventListener('click', checarPalpite);
@@ -65,9 +76,20 @@ function reinicioJogo() {
     userPalpite.value = '';
     tentativaAtual = 1;
     botaoReinicio.disabled = true;
-    
+
 
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
